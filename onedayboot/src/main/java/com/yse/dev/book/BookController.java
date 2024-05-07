a7c2992554d47c1c754622001c8d1c0413fdcdab
Inchan kim <kiminchan12341@gmail.com>
2024-05-07 18:53:55 +0900

    first project commit

---
+++ onedayboot/src/main/java/com/yse/dev/book/BookController.java
@@ -0,0 +1,13 @@
+package com.yse.dev.book;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+	
+	@GetMapping("/book/create")
+	public String create() {
+		return "book/create";
+	}
+}
---
+++ onedayboot/src/main/java/com/yse/dev/entity/BookRepository.java
@@ -0,0 +1,7 @@
+package com.yse.dev.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+}
